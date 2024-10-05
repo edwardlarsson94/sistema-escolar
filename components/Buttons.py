4c53090ae26b0f54cc7cbee8eef7344e64aa7481
Angela <angemervega@gmail.com>
2024-10-05 23:03:31 +0200

    0014 add translated text

--- components/Buttons.py
+++ components/Buttons.py
@@ -8,7 +8,7 @@ def on_leave(e):
     e.widget['background'] = BUTTON_COLOR
 
 def create_login_button(window, command, entry_user, entry_password):
-    button_login = tk.Button(window, text="Sign in", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, font=("Helvetica", 12, "bold"), relief="flat")
+    button_login = tk.Button(window, text="¡Ingresar!", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, font=("Helvetica", 12, "bold"), relief="flat")
     button_login.pack(pady=20, ipadx=80, ipady=10)
 
     button_login.config(command=command)
--- src/modules/login/views/Login.py
+++ src/modules/login/views/Login.py
@@ -7,7 +7,7 @@ from src.modules.home.Home import show_home_view
 from components.Buttons import create_login_button
 
 def create_login_view(window):
-    window.title("Welcome Back!")
+    window.title("Sistema Escolar")
     window.geometry("400x600")
     window.configure(bg=BACKGROUND_COLOR)
 
@@ -23,8 +23,8 @@ def create_login_view(window):
     title_font = font.Font(family="Helvetica", size=24, weight="bold")
     subtitle_font = font.Font(family="Helvetica", size=10)
 
-    label_title = tk.Label(window, text="Welcome Back!", bg=BACKGROUND_COLOR, fg=TITLE_COLOR, font=title_font)
-    label_subtitle = tk.Label(window, text="Welcome back, we missed you", bg=BACKGROUND_COLOR, fg=SUBTITLE_COLOR, font=subtitle_font)
+    label_title = tk.Label(window, text="Sistema Escolar", bg=BACKGROUND_COLOR, fg=TITLE_COLOR, font=title_font)
+    label_subtitle = tk.Label(window, text="Ahora puedes iniciar seccion!!", bg=BACKGROUND_COLOR, fg=SUBTITLE_COLOR, font=subtitle_font)
 
     def adjust_positions():
         window_width = window.winfo_width()
@@ -33,13 +33,13 @@ def create_login_view(window):
 
     window.after(100, adjust_positions)
 
-    label_user = tk.Label(window, text="Username:", bg=BACKGROUND_COLOR, fg=LABEL_COLOR, font=("Helvetica", 12))
+    label_user = tk.Label(window, text="Usuario:", bg=BACKGROUND_COLOR, fg=LABEL_COLOR, font=("Helvetica", 12))
     label_user.pack(pady=10)  
 
     entry_user = tk.Entry(window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, font=("Helvetica", 12), insertbackground=ENTRY_FOREGROUND, relief="flat")
     entry_user.pack(ipady=8, ipadx=10, pady=10) 
 
-    label_password = tk.Label(window, text="Password:", bg=BACKGROUND_COLOR, fg=LABEL_COLOR, font=("Helvetica", 12))
+    label_password = tk.Label(window, text="Contraseña:", bg=BACKGROUND_COLOR, fg=LABEL_COLOR, font=("Helvetica", 12))
     label_password.pack(pady=10) 
     
     entry_password = tk.Entry(window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, show="*", font=("Helvetica", 12), insertbackground=ENTRY_FOREGROUND, relief="flat")
