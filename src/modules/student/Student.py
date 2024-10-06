fe3cbd3aee32a7b2f0f6fe516b37be537e2358b9
edward vega <edward.vega@wiedii.co>
2024-10-06 11:22:28 -0500

    0023 add funtion delete student

--- src/modules/student/Student.py
+++ src/modules/student/Student.py
@@ -40,8 +40,33 @@ def open_edit_student_form(tree, selected_item, student_data):
     save_button.bind("<Enter>", on_enter)
     save_button.bind("<Leave>", on_leave)
 
-def delete_student(student_id):
-    print(f"Borrar estudiante con cédula {student_id}")
+def confirm_delete_student(tree, selected_item, student_id):
+    new_window = tk.Toplevel()
+    new_window.title("Confirmar Borrado")
+    new_window.geometry("300x150")
+    new_window.configure(bg=BACKGROUND_COLOR)
+
+    label_message = tk.Label(new_window, text=f"¿Estás seguro de que deseas borrar al estudiante con cédula {student_id}?", 
+                             bg=BACKGROUND_COLOR, fg=TITLE_COLOR, wraplength=250)
+    label_message.pack(pady=10)
+
+    def delete_confirmed():
+        tree.delete(selected_item)
+        print(f"Estudiante con cédula {student_id} borrado")
+        new_window.destroy()
+
+    button_confirm = tk.Button(new_window, text="Confirmar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
+                               command=delete_confirmed)
+    button_cancel = tk.Button(new_window, text="Cancelar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
+                              command=new_window.destroy)
+
+    button_confirm.bind("<Enter>", on_enter)
+    button_confirm.bind("<Leave>", on_leave)
+    button_cancel.bind("<Enter>", on_enter)
+    button_cancel.bind("<Leave>", on_leave)
+
+    button_confirm.pack(side="left", padx=20, pady=20)
+    button_cancel.pack(side="right", padx=20, pady=20)
 
 def on_enter(e):
     e.widget['background'] = BUTTON_COLOR_HOVER
@@ -56,7 +81,7 @@ def on_click_action(tree, edit_button, delete_button):
         student_id = selected_student[0]
 
         edit_button.config(state="normal", command=lambda: open_edit_student_form(tree, selected_item, selected_student))
-        delete_button.config(state="normal", command=lambda: delete_student(student_id))
+        delete_button.config(state="normal", command=lambda: confirm_delete_student(tree, selected_item, student_id))
 
 def open_new_student_form(tree):
     new_window = tk.Toplevel()
