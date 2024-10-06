517d9b64ce46899d48ddd716b1076957ab8881ff
Angela <angemervega@gmail.com>
2024-10-06 22:13:52 +0200

    0025 change texts and teachers

--- constants/Texts.py
+++ constants/Texts.py
@@ -1,15 +1,24 @@
-LOGIN_SISTEMA_ESCOLAR_TITLE = "Sistema Escolar"
+
 LOGIN_SISTEMA_ESCOLAR_SUBTITLE = "Ahora puedes iniciar sesión!!"
 LOGIN_LABEL_USER = "Usuario:"
 LOGIN_LABEL_PASSWORD = "Contraseña:"
 LOGIN_BUTTON_LOGIN = "¡Ingresar!"
 
-HOME_TITLE = "Sistema Escolar"
 HOME_SUBTITLE = "Bienvenido a Sistema Escolar"
-HOME_BUTTON_AGREGAR_ESTUDIANTE = "Agregar Estudiante"
 HOME_BUTTON_EDITAR = "Editar"
 HOME_BUTTON_BORRAR = "Borrar"
 
-STUDENT_TABLE_CEDULA = "Cédula"
-STUDENT_TABLE_NOMBRE = "Nombre"
-STUDENT_BUTTON_GUARDAR = "Guardar"
+
+STUDENT_TITLE_EDITAR = "Editar Estudiante"
+STUDENT_TITLE_AGREGAR = "Agregar Estudiante"
+
+GLO_TITLE_EDITAR = "Editar Docente"
+DOCENTE_TITLE_AGREGAR = "Agregar Docente"
+
+GLOBAL_SISTEMA_ESCOLAR_TITLE = "Sistema Escolar"
+GLOBAL_CONFIRMAR_BORRAR = "Confirmar Borrado"
+GLOBAL_BUTTON_GUARDAR = "Guardar"
+GLOBAL_BUTTON_CONFIRMAR = "Confirmar"
+GLOBAL_BUTTON_CANCELAR ="Cancelar"
+GLOBAL_TABLE_CEDULA = "Cédula"
+GLOBAL_TABLE_NOMBRE = "Nombre"
--- src/modules/teachers/teachers.py
+++ src/modules/teachers/teachers.py
@@ -1,60 +1,60 @@
 import tkinter as tk
 from constants.Colors import (BACKGROUND_COLOR, TITLE_COLOR, BUTTON_COLOR, BUTTON_TEXT_COLOR, BUTTON_COLOR_HOVER, ENTRY_BACKGROUND, ENTRY_FOREGROUND)
-from constants.Texts import (STUDENT_TITLE_EDITAR, STUDENT_TITLE_AGREGAR, STUDENT_TITLE_CONFIRMAR_BORRAR, STUDENT_TABLE_CEDULA, STUDENT_TABLE_NOMBRE, GLOBAL_BUTTON_GUARDAR, STUDENT_TABLE_CONFIRMAR, STUDENT_BUTTON_CONFIRMAR, STUDENT_BUTTON_CANCELAR)
+from constants.Texts import (DOCENTE_TABLE_CEDULA, DOCENTE_TABLE_NOMBRE, GLOBAL_BUTTON_GUARDAR, GLOBAL_TITLE_EDITAR)
 
-def add_student(tree, cedula, nombre, new_window):
+def add_docente(tree, cedula, nombre, new_window):
     tree.insert("", "end", values=(cedula, nombre))
-    print(f"Nuevo estudiante agregado con cédula: {cedula} y nombre: {nombre}")
+    print(f"Nuevo docente agregado con cédula: {cedula} y nombre: {nombre}")
     new_window.destroy()
 
-def update_student(tree, selected_item, cedula, nombre, new_window):
+def update_docente(tree, selected_item, cedula, nombre, new_window):
     tree.item(selected_item, values=(cedula, nombre))
-    print(f"Estudiante actualizado con cédula: {cedula} y nombre: {nombre}")
+    print(f"Docente actualizado con cédula: {cedula} y nombre: {nombre}")
     new_window.destroy()
 
-def open_edit_student_form(tree, selected_item, student_data):
+def open_edit_docente_form(tree, selected_item, docente_data):
     new_window = tk.Toplevel()
-    new_window.title(STUDENT_TITLE_EDITAR)
+    new_window.title(GLOBAL_TITLE_EDITAR)
     new_window.geometry("300x200")
     new_window.configure(bg=BACKGROUND_COLOR)
 
-    label_cedula = tk.Label(new_window, text=STUDENT_TABLE_CEDULA, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
+    label_cedula = tk.Label(new_window, text=DOCENTE_TABLE_CEDULA, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
     label_cedula.pack(pady=5)
     entry_cedula = tk.Entry(new_window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, relief="flat")
-    entry_cedula.insert(0, student_data[0])
+    entry_cedula.insert(0, docente_data[0])
     entry_cedula.pack(pady=5)
 
-    label_nombre = tk.Label(new_window, text=STUDENT_TABLE_NOMBRE, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
+    label_nombre = tk.Label(new_window, text=DOCENTE_TABLE_NOMBRE, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
     label_nombre.pack(pady=5)
     entry_nombre = tk.Entry(new_window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, relief="flat")
-    entry_nombre.insert(0, student_data[1])
+    entry_nombre.insert(0, docente_data[1])
     entry_nombre.pack(pady=5)
 
     save_button = tk.Button(new_window, text=GLOBAL_BUTTON_GUARDAR, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat", 
-                            command=lambda: update_student(tree, selected_item, entry_cedula.get(), entry_nombre.get(), new_window))
+                            command=lambda: update_docente(tree, selected_item, entry_cedula.get(), entry_nombre.get(), new_window))
     save_button.pack(pady=10)
 
     save_button.bind("<Enter>", on_enter)
     save_button.bind("<Leave>", on_leave)
 
-def confirm_delete_student(tree, selected_item, student_id):
+def confirm_delete_docente(tree, selected_item, docente_id):
     new_window = tk.Toplevel()
-    new_window.title(STUDENT_TITLE_CONFIRMAR_BORRAR)
+    new_window.title("Confirmar Borrado")
     new_window.geometry("300x150")
     new_window.configure(bg=BACKGROUND_COLOR)
 
-    label_message = tk.Label(new_window, text=f"¿Estás seguro de que deseas borrar al estudiante con cédula {student_id}?", 
+    label_message = tk.Label(new_window, text=f"¿Estás seguro de que deseas borrar al docente con cédula {docente_id}?", 
                              bg=BACKGROUND_COLOR, fg=TITLE_COLOR, wraplength=250)
     label_message.pack(pady=10)
 
     def delete_confirmed():
         tree.delete(selected_item)
-        print(f"Estudiante con cédula {student_id} borrado")
+        print(f"Docente con cédula {docente_id} borrado")
         new_window.destroy()
 
-    button_confirm = tk.Button(new_window, text=STUDENT_TABLE_CONFIRMAR, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
+    button_confirm = tk.Button(new_window, text="Confirmar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
                                command=delete_confirmed)
-    button_cancel = tk.Button(new_window, text=STUDENT_BUTTON_CANCELAR, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
+    button_cancel = tk.Button(new_window, text="Cancelar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, 
                               command=new_window.destroy)
 
     button_confirm.bind("<Enter>", on_enter)
@@ -74,31 +74,33 @@ def on_leave(e):
 def on_click_action(tree, edit_button, delete_button):
     selected_item = tree.selection()
     if selected_item:
-        selected_student = tree.item(selected_item, 'values')
-        student_id = selected_student[0]
+        selected_docente = tree.item(selected_item, 'values')
+        docente_id = selected_docente[0]
 
-        edit_button.config(state="normal", command=lambda: open_edit_student_form(tree, selected_item, selected_student))
-        delete_button.config(state="normal", command=lambda: confirm_delete_student(tree, selected_item, student_id))
+        edit_button.config(state="normal", command=lambda: open_edit_docente_form(tree, selected_item, selected_docente))
+        delete_button.config(state="normal", command=lambda: confirm_delete_docente(tree, selected_item, docente_id))
 
-def open_new_student_form(tree):
+def open_new_docente_form(tree):
     new_window = tk.Toplevel()
-    new_window.title(STUDENT_TITLE_AGREGAR)
+    new_window.title("Agregar Docente")
     new_window.geometry("300x200")
     new_window.configure(bg=BACKGROUND_COLOR)
 
-    label_cedula = tk.Label(new_window, text=STUDENT_TABLE_CEDULA, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
+    label_cedula = tk.Label(new_window, text=DOCENTE_TABLE_CEDULA, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
     label_cedula.pack(pady=5)
     entry_cedula = tk.Entry(new_window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, relief="flat")
     entry_cedula.pack(pady=5)
 
-    label_nombre = tk.Label(new_window, text=STUDENT_TABLE_NOMBRE, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
+    label_nombre = tk.Label(new_window, text=DOCENTE_TABLE_NOMBRE, bg=BACKGROUND_COLOR, fg=TITLE_COLOR)
     label_nombre.pack(pady=5)
     entry_nombre = tk.Entry(new_window, bg=ENTRY_BACKGROUND, fg=ENTRY_FOREGROUND, relief="flat")
     entry_nombre.pack(pady=5)
 
     save_button = tk.Button(new_window, text=GLOBAL_BUTTON_GUARDAR, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat", 
-                            command=lambda: add_student(tree, entry_cedula.get(), entry_nombre.get(), new_window))
+                            command=lambda: add_docente(tree, entry_cedula.get(), entry_nombre.get(), new_window))
     save_button.pack(pady=10)
 
     save_button.bind("<Enter>", on_enter)
     save_button.bind("<Leave>", on_leave)
+    
+    
