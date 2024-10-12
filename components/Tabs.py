3e0a664690fcd6a7e3d2efca0cad0e4bf03a95e4
edward vega <edward.vega@wiedii.co>
2024-10-11 19:13:20 -0500

    0041 add styles header

--- components/Tabs.py
+++ components/Tabs.py
@@ -3,7 +3,7 @@ from constants.Colors import BACKGROUND_COLOR, BUTTON_COLOR, BUTTON_TEXT_COLOR, 
 from constants.Texts import HOME_SUBTITLE, GLOBAL_STUDENT_TITLE_ADD, HOME_BUTTON_DELETE, HOME_BUTTON_EDIT, TEACHER_TITLE_ADD, GLOBAL_STUDENT,GLOBAL_TEACHER
 from components.Table import create_student_table, populate_table, bind_row_selection
 from src.modules.student.Student import open_new_student_form, on_click_action as on_student_click_action
-from src.modules.teachers.Teachers import open_new_teacher_form, on_click_action as on_teacher_click_action
+from src.modules.teachers.views.Teachers import open_new_teacher_form, on_click_action as on_teacher_click_action
 
 def create_student_tab(notebook):
     student_tab = tk.Frame(notebook, bg=BACKGROUND_COLOR)
--- src/modules/home/Home.py
+++ src/modules/home/Home.py
@@ -9,16 +9,12 @@ def show_home_view():
     home_window.geometry("600x500")
     home_window.configure(bg=BACKGROUND_COLOR)
 
-    logout_button = tk.Button(home_window, text="Cerrar Sesión", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat",  command=lambda: logout(home_window))
-    logout_button.pack(side="top", anchor="ne", padx=10, pady=10)
-    
-    #button de busqueda
-    
-    search_button = tk.Button(home_window, text="Buscar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat", command=search_action)  # Se define la acción del botón de búsqueda
-    search_button.pack(side="top", anchor="nw", padx=10, pady=10)
-
-    #button de busqueda
-    
+    top_frame = tk.Frame(home_window, bg=BACKGROUND_COLOR)
+    top_frame.pack(side="top", fill="x", padx=10, pady=10)
+    logout_button = tk.Button(top_frame, text="Cerrar Sesión", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat", command=lambda: logout(home_window))
+    logout_button.pack(side="right", padx=5)
+    search_button = tk.Button(top_frame, text="Buscar", bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, relief="flat", command=search_action)
+    search_button.pack(side="left", padx=5)
     notebook = ttk.Notebook(home_window)
     notebook.pack(expand=True, fill='both')
 
@@ -33,8 +29,5 @@ def logout(window):
     root = tk.Tk()
     create_login_view(root)
     root.mainloop()
-    
-    
-    #funcion de button de busqueda
 def search_action():
     print("Función de búsqueda ejecutada.")
